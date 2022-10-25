66e8879d6f63bbbf6c53dba188be056d53f3a21c
badman-dev <budmandev@gmail.com>
2022-10-24 22:45:01 -0400

    solo modals

--- modalstuff.js
+++ modalstuff.js
@@ -4,6 +4,7 @@ var modal = document.getElementById("myModal");
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("myImg");
 var images = document.getElementsByClassName("modal-image");
+var soloImages = document.getElementsByClassName("round-image");
 var modalImg = document.getElementById("img01");
 
 var span = document.getElementsByClassName("close")[0];
@@ -14,8 +15,18 @@ var rightButton = document.getElementById("rightBtn");
 
 function openModal(id){
     modal.style.display = "block";
+    leftButton.style.display = "inline-block";
+    rightButton.style.display = "inline-block";
     // modalImg.src = this.src;
     setModalImage(id);
+}
+
+function openSoloModal(id){
+    modal.style.display = "block";
+    leftButton.style.display = "none";
+    rightButton.style.display = "none";
+
+    modalImg.src = soloImages[id].src;
 }
 
 function setModalImage(id) {
@@ -24,7 +35,6 @@ function setModalImage(id) {
     } else if (id >= images.length) {
         id = 0;
     }
-    console.log("set modal image");
     currentImage = id;
     modalImg.src = images[id].src;
 }
@@ -34,6 +44,12 @@ for (i = 0; i < images.length; i++){
     // images[i].onclick = function() { openModal(i); };
     (function (i) {
          images[i].onclick = function() { openModal(i); };
+    })(i);
+}
+
+for (i = 0; i < soloImages.length; i++){
+    (function (i) {
+         soloImages[i].onclick = function() { openSoloModal(i); };
     })(i);
 }
 
